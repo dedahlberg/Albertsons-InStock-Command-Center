4c0a1508bc386af2eb1aa9a78625fc2335e562d7
dedahlberg <dwaynedahlberg@gmail.com>
2026-08-20 07:34:18 -0500

    Add first known-good CJ Schwan's product master

---
+++ data/products-cjs.js
@@ -0,0 +1,5 @@
+window.CJS_PRODUCTS = [
+  {brand:'Red Baron',category:'Pizza',name:'Red Baron Pizza Deep Dish Singles Pepperoni 2 Count - 11.2 Oz',productId:'960516496'},
+  {brand:'bibigo',category:'Asian',name:'Bibigo Pork & Vegetable Mini Wontons - 24 Oz',productId:'960225277',upc:'0080717671171'},
+  {brand:'bibigo',category:'Asian',name:'Bibigo Chicken & Vegetable Mini Wontons - 24 Oz',productId:'960225276'}
+];
