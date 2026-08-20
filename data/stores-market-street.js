eb4fe7de6391ab64bf01a3993ca02ac7372b16a7
dedahlberg <dwaynedahlberg@gmail.com>
2026-08-20 07:10:09 -0500

    Add Market Street banner stores to Texas master

---
+++ data/stores-market-street.js
@@ -0,0 +1,5 @@
+window.MARKET_STREET_STORES = [
+  {banner:'Market Street',store:'MARKET STREET',address:'3800 LONG PRAIRIE RD',city:'FLOWER MOUND',chain:'UNITED',market:'Dallas',zone:'Texas',longitude:-97.0678272,latitude:33.0375005,deliveryDays:['Wed']},
+  {banner:'Market Street',store:'685',address:'4268 LEGACY DR',city:'FRISCO',chain:'UNITED',market:'Dallas',zone:'Texas',longitude:-96.8376442,latitude:33.1146838,deliveryDays:['Wed']},
+  {banner:'Market Street',store:'696',address:'3141 E BROAD ST',city:'MANSFIELD',chain:'UNITED',market:'Dallas',zone:'Texas',longitude:-97.0887169,latitude:32.5757286,deliveryDays:['Mon']}
+];
